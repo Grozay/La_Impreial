08fe899e3bfe10e186305fc62eadce01355acddd
Zess0503 <quanyaya2007@gmail.com>
2023-11-25 13:37:04 +0700

    Update ProductItem.js

--- source/src/components/Products/ProductItem.js
+++ source/src/components/Products/ProductItem.js
@@ -1,1 +1,27 @@
 //item
+import { useNavigate } from "react-router-dom";
+
+
+function ProductItem({ product }) {
+   
+    return (
+        <div >
+
+            <div>
+                <h3>{product.name}</h3>
+                <p>{product.id}</p>
+                <p>{product.brand}</p>
+                <p>{product.type}</p>
+                <p>${product.price}</p>
+                <p>{product.quantity}</p>
+                {/* <p>{product.description}</p> */}
+            </div>
+
+
+
+            
+        </div>
+
+    );
+}
+export default ProductItem;
