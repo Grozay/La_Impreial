72e7b3748213cd1c45c2adf0f97c592f8b088d97
Quyen <146425082+Grozay@users.noreply.github.com>
2023-11-25 15:43:40 +0700

    add

--- source/src/components/Products/ProductItem.js
+++ source/src/components/Products/ProductItem.js
@@ -10,8 +10,8 @@ function ProductItem({ product }) {
             <div>
                 <h3>{product.name}</h3>
                 <p>{product.price}$</p>
-                <p>{product.quantity}</p>
-                <img src={"../assets/" + product.image[0]} />
+                <p>quantity:{product.quantity}</p>
+                <img src={product.image[0]} width="100px" />
 
             </div>
         </div>
