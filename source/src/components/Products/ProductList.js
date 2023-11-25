ae78e711a5467c2af010ef930d88354dba9c6f02
Zess0503 <quanyaya2007@gmail.com>
2023-11-25 13:37:07 +0700

    Update ProductList.js

--- source/src/components/Products/ProductList.js
+++ source/src/components/Products/ProductList.js
@@ -1,1 +1,13 @@
 //list
+import ProductItem from "./ProductItem"
+function ProductList({ products }) {
+    return (
+        <div>
+            <h3>Product List</h3>
+            {products.map(pro => (
+                <ProductItem key={pro.id} product={pro}  />
+            ))}
+        </div>
+    )
+}
+export default ProductList;
