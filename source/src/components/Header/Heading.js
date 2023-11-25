976b3cc5bf4a442d25a5abd175c12b30c4f3e7b0
Quyen <146425082+Grozay@users.noreply.github.com>
2023-11-25 22:00:50 +0700

    a

--- source/src/components/Header/Heading.js
+++ source/src/components/Header/Heading.js
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom'
 import '../../css/Heading/Heading.css'
 const Heading = () => {
     return (
-        <div className='full'>
+        <div className='boxHeading'>
             <div className="heading">
                 <div>
                     this is Header
                 </div>
-                <Link to="/account" className='myAccount'>My Account</Link>
+                <div>
+                    <Link to="/account" className='myAccount'>My Account</Link>
+                </div>
             </div>
             <hr />
         </div>
