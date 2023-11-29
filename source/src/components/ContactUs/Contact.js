d619d577714f328671d77caf3ac0e35cf9dc118d
Zess0503 <quanyaya2007@gmail.com>
2023-11-29 14:28:06 +0700

    Update Contact.js

--- source/src/components/ContactUs/Contact.js
+++ source/src/components/ContactUs/Contact.js
@@ -59,12 +59,12 @@ function Contact({onAdd}){
                       <h2>Contact Us</h2>
                         <h5>La_Imperial (by Thieu Nu Group)</h5>
                         <li className='header_bavbar_item'>
-                            <i class="fa-solid fa-phone"></i>
-                                  Tel: 09123456789
+                            <i class="fa-solid fa-phone" >Tel: 09123456789</i>
+                                  
                         </li>
                         <p>
-                            Email
-                            <a href="mailto:thieunu@gmail.com">thieunu@gmail.com</a>
+                            Email: 
+                            <a href="mailto:thieunu@gmail.com" style={{textDecoration: "none"}}> thieunu@gmail.com</a>
                         </p>
                         {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2375.302031394123!2d-2.2939150236934203!3d53.4630620659846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bae72e7e47f69%3A0x6c930e96df4455fe!2zU8OibiB24bqtbiDEkeG7mW5nIE9sZCBUcmFmZm9yZA!5e0!3m2!1svi!2s!4v1701101284365!5m2!1svi!2s" width="400" height="300"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2375.302031394123!2d-2.2939150236934203!3d53.4630620659846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bae72e7e47f69%3A0x6c930e96df4455fe!2zU8OibiB24bqtbiDEkeG7mW5nIE9sZCBUcmFmZm9yZA!5e0!3m2!1svi!2s!4v1701240441849!5m2!1svi!2s" width="550" height="400"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
