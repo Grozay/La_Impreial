e2570750d84406d99b530dba52c33d61e364f92d
Quyen <146425082+Grozay@users.noreply.github.com>
2023-12-01 11:07:44 +0700

    Update App.js

--- source/src/App.js
+++ source/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css'
 import ProductList from './components/Products/ProductList';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/AppBar/Nav';
