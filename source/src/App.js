7472bc38e9b32e8c6b8c085bbd4a3d37885d4869
Zess0503 <quanyaya2007@gmail.com>
2023-12-05 15:42:31 +0700

    Update App.js

--- source/src/App.js
+++ source/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
 import ProductList from './components/Products/ProductList';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import NavBar from './components/AppBar/Nav';
 import HomePage from './Home/HomePage/HomePage';
 import Heading from './components/Header/Heading';
@@ -11,6 +11,8 @@ import Panasonic from './Home/Brand/Panasonic';
 import Toshiba from './Home/Brand/Toshiba';
 import Contact from './components/ContactUs/Contact';
 import ProductDentail from './components/Products/ProductDetail';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 // import Cursor from './components/Cursor/Cursor'
 
 function App() {
@@ -25,6 +27,79 @@ function App() {
   const [productQuality, setProductQuality] = useState([])
   // const [searchValue, setSearchValue] = useState('');
   const [noResults, setNoResults] = useState(false);
+
+
+  const [users, setUser] = useState([]);
+  const homepage = useNavigate();
+  const [erroLogin, setErrorLogin] = useState('');
+
+
+  useEffect(() => {
+    fetch('/data/user.json')
+      .then(response1 => response1.json())
+      .then(data1 => {
+        const localUsers = getUsersFromLocal();
+        setUser(data1);
+      })
+      .catch(error => console.log('error reading json', error));
+  }, []);
+
+//login 
+const checkLogin = (checkUser) => {
+  const findUser = users.find(u => u.username === checkUser.username && u.password === checkUser.password);
+  if (findUser != null) {
+    setErrorLogin('');
+    saveUsersToLocal([...users, checkUser]);
+    localStorage.setItem('username', checkUser.username);
+    homepage('/products/')
+  } else {
+    setErrorLogin('Invalid username or password')
+  }
+}
+//register
+const handleRegister = (newUser) => {
+  setUser([...users, newUser]);
+  // saveUsersToFile([...users, newUser]);
+  saveUsersToLocal([...users, newUser]);
+  console.log('User registered successfully');
+};
+
+//Login = API
+// const saveUsersToFile = (data) => {
+//   // Save data to user.json or an API endpoint
+//   fetch('/data/user.json', {
+//     method: 'PUT', // or 'POST' if you prefer
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   })
+//     .then((response) => response.json())
+//     .then((result) => console.log('User data saved:', result))
+//     .catch((error) => console.error('Error saving user data', error));
+// };
+
+//cach 2
+const saveUsersToLocal = (data) => {
+  try {
+    localStorage.setItem('userData', JSON.stringify(data));
+    console.log('User data saved successfully');
+  } catch (error) {
+    console.error('Error saving user data', error);
+  }
+};
+
+const getUsersFromLocal = () => {
+  try {
+    return JSON.parse(localStorage.getItem('userData')) || [];
+  } catch (error) {
+    console.error('Error retrieving user data', error);
+    return [];
+  }
+};
+
+
+
 
   useEffect(() => {
     fetch('/data/products.json')
@@ -83,6 +158,9 @@ function App() {
   };
 
 
+
+  
+
   return (
     <div className="App">
       <Heading />
@@ -95,11 +173,14 @@ function App() {
         <Route
           path="/products"
           element={
-
+            localStorage.getItem('username') ? (
             noResults ? (
               <p className='no-results-message'>No Found Products</p>
             ) : (
               <ProductList products={filterProduct} />
+            )
+            ) : (
+              <Navigate to='/account' />
             )
           }
         />
@@ -108,6 +189,8 @@ function App() {
         <Route path="/toshiba" element={<Toshiba toshibaProduct={toshibaProduct} />} />
         <Route path="/contact" element={<Contact onAdd={handleAdd} />} />
         <Route path='/products/:id' element={<ProductDentail productDentail={filterProduct} />} />
+        <Route path='/account' element={<Login checkLogin={checkLogin} erroLogin={erroLogin} />} />
+        <Route path="/register" element={<Register onRegister={handleRegister} />} />
       </Routes>
       {/* <div>
         <Cursor />
