293034a73c2d859fd8af530d070898fbf9c6890c
Zess0503 <quanyaya2007@gmail.com>
2023-12-19 10:30:47 +0700

    Update Contact.js

--- source/src/components/ContactUs/Contact.js
+++ source/src/components/ContactUs/Contact.js
@@ -1,5 +1,7 @@
 import '../../css/Contact/contact.css'
 import { useState } from "react";
+import Swal from 'sweetalert2' ;
+import {useNavigate, Navigate} from 'react-router-dom';
 function Contact({onAdd}){
     
   const [name, setName] = useState('');
@@ -9,47 +11,90 @@ function Contact({onAdd}){
   const [message, setMessage] = useState('');
 
 
+
+  const [errorName, setErrorName] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [errorContact, setErrorContact] = useState('');
+  const [errorSubject, setErrorSubjiect] = useState('');
+  
+  const homepage = useNavigate();
+  const MainAlert = Swal.mixin({
+    showConfirmButton: false,
+    timer: 1000,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Check if all required fields are filled
-    if (name && email && contact && subject) {
-      const contacts = { name, email, contact, subject, message };
+
+    if(!name){
+      setErrorName('Name is required');
+  }
+  else if(name){
+    setErrorName('');
+  }
+   if(!email){
+    setErrorEmail('Email is required');
+  }
+  else if(email){
+    setErrorEmail('');
+  }
+  if(!contact){
+    setErrorContact('Contact is required');
+  }
+  else if(contact){
+    setErrorContact('');
+  }
+  if(!subject){
+    setErrorSubjiect('Subject is required');
+  }
+  else if(subject){
+    setErrorSubjiect('');
+    {
+      // Display an error message
+      MainAlert.fire({
+        icon: 'success',
+        timer: 1500,
+        title: 'Thank You For Your Contact',
+      });
+      homepage('/products')
+
+  }
+  }else{
+    const contacts = { name, email, contact, subject, message };
       onAdd(contacts);
       setName('');
       setEmail('');
       setContact('');
       setSubject('');
       setMessage('');
-      alert('Your message has been sent');
-    } else {
-      alert('Please fill in all required fields.');
-    }
+  }
+   
+
+
+
+    // if (name && email && contact && subject) {
+    //   const contacts = { name, email, contact, subject, message };
+    //   onAdd(contacts);
+    //   setName('');
+    //   setEmail('');
+    //   setContact('');
+    //   setSubject('');
+    //   setMessage('');
+    //   alert('Your message has been sent');
+    // } else {
+    //   alert('Please fill in all required fields.');
+    // }
   };
 
 
 
 
 
-
-    // const [formData, setFormData] = useState({
-    //     name: '',
-    //     email: '',
-    //     contact: '',
-    //     subject: '',
-    //     message: '',
-    //   });
-
-    //   const handleChange = (e) => {
-    //     setFormData({ ...formData, [e.target.name]: e.target.value });
-    //   };
-
-    //   const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     // Handle form submission logic here
-    //     console.log('Form data submitted:', formData);
-    //     // You can add an API call or other logic for handling form data
-    //   };
 
     return(
 <div className="container1">
@@ -68,7 +113,7 @@ function Contact({onAdd}){
                         </p>
                         {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2375.302031394123!2d-2.2939150236934203!3d53.4630620659846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bae72e7e47f69%3A0x6c930e96df4455fe!2zU8OibiB24bqtbiDEkeG7mW5nIE9sZCBUcmFmZm9yZA!5e0!3m2!1svi!2s!4v1701101284365!5m2!1svi!2s" width="400" height="300"   allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
                         {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2375.302031394123!2d-2.2939150236934203!3d53.4630620659846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bae72e7e47f69%3A0x6c930e96df4455fe!2zU8OibiB24bqtbiDEkeG7mW5nIE9sZCBUcmFmZm9yZA!5e0!3m2!1svi!2s!4v1701240441849!5m2!1svi!2s" width="550" height="400"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.3470122242056!2d106.68668417451731!3d10.784712059041894!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f321946a98b%3A0x327b97d03fbb903f!2zMzkxIMSQLiBOYW0gS-G7syBLaOG7n2kgTmdoxKlhLCBWw7UgVGjhu4sgU8OhdSwgUXXhuq1uIDMsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1702623599143!5m2!1svi!2s" width="550" height="400"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3919.266255370532!2d106.682426!3d10.790908000000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317529cfdaf8731f%3A0x1628c44dc1f38869!2zMzkxYSDEkC4gTmFtIEvhu7MgS2jhu59pIE5naMSpYSwgUGjGsOG7nW5nIFZUUywgUXXhuq1uIDMsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2sus!4v1702894416101!5m2!1svi!2sus" width="550" height="400"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
       
@@ -85,18 +130,22 @@ function Contact({onAdd}){
                         <div class="input">
                           <h2 style={{ fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif', fontWeight: 'normal' }}>Your Name</h2>
                             <input type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                            <div className="Register_error">{errorName}</div>
                         </div>
                         <div class="input">
                           <h2 style={{ fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif', fontWeight: 'normal' }}>Your Email</h2>
                             <input type="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <div className="Register_error">{errorEmail}</div>
                         </div>
                         <div class="input">
                           <h2 style={{ fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif', fontWeight: 'normal' }}>Your Contact:</h2>
                             <input type="text" placeholder="Your Contact" value={contact} onChange={(e) => setContact(e.target.value)} />
+                            <div className="Register_error">{errorContact}</div>
                         </div>
                         <div class="input">
                           <h2 style={{ fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif', fontWeight: 'normal' }}>Subject</h2>
                             <input type="text" placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
+                            <div className="Register_error">{errorSubject}</div>
                         </div>
                         <div class="input">
                           <h2 style={{ fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif', fontWeight: 'normal' }}>Your message (optional)</h2>
