d538805d3915788c863fe12328fa78db9247420f
Zess0503 <quanyaya2007@gmail.com>
2023-12-19 10:31:04 +0700

    Update index.js

--- source/src/index.js
+++ source/src/index.js
@@ -4,8 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom';
+
 import Footer from './components/Footer/Footer';
 import UnderFooter from './components/Footer/UnderFooter';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
